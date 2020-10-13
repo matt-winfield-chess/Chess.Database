3850a38de68c6b51dc55c47511b3b00c18757c2f
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2020-10-13 13:18:50 +0100

    Add stored proc for getting user credentials

---
+++ flyway-7.0.0/sql/V4__Get_credentials_stored_proc.sql
@@ -0,0 +1,22 @@
+DELIMITER $$
+
+CREATE PROCEDURE GetUserCredentialsByUsername(
+	IN usernameInput VARCHAR(255),
+	OUT idOutput INT,
+	OUT passwordHashOutput VARCHAR(255),
+	OUT passwordSaltOutput VARCHAR(255)
+)
+BEGIN
+
+	SELECT
+		Id, PasswordHash, PasswordSalt
+	INTO
+		idOutput, passwordHashOutput, passwordSaltOutput
+	FROM
+		Users
+	WHERE
+		Username = usernameInput;
+
+END$$
+
+DELIMITER ;
