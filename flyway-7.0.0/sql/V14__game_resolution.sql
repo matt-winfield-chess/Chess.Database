1031f3051cfe8d09b74a9fcfbf1db90b020b5eb7
Matthew Winfield <matthew.thomas.winfield@gmail.com>
2020-11-04 10:10:03 +0000

    Add game resolution to games table

---
+++ flyway-7.0.0/sql/V14__game_resolution.sql
@@ -0,0 +1,45 @@
+ALTER TABLE Games
+	ADD COLUMN Winner VARCHAR(255),
+	ADD COLUMN WinnerId INT,
+	ADD COLUMN Termination VARCHAR(1000),
+	ADD FOREIGN KEY (WinnerId) REFERENCES Users(Id) ON DELETE SET NULL;
+
+DROP PROCEDURE GetGameById;
+
+DELIMITER $$
+
+CREATE PROCEDURE GetGameById(
+	IN idInput VARCHAR(255)
+)
+BEGIN
+
+	SELECT
+		Id, WhitePlayerId, BlackPlayerId, Active, Fen, Winner, WinnerId, Termination
+	FROM
+		Games
+	WHERE
+		Games.Id = idInput;
+
+END$$
+
+CREATE PROCEDURE SetGameWinner(
+	IN gameIdInput VARCHAR(255),
+	IN winnerColorInput VARCHAR(255),
+	IN winnerIdInput INT,
+	IN terminationInput VARCHAR(1000)
+)
+BEGIN
+
+	UPDATE
+		Games
+	SET
+		Games.Winner=winnerColorInput,
+		Games.WinnerId=winnerIdInput,
+		Games.Termination=terminationInput,
+		Games.Active=FALSE
+	WHERE
+		Games.Id = gameIdInput;
+
+END$$
+
+DELIMITER ;
